96dd4b1d9663fb907c4874650cb907acc2777ab2
Neftali Acosta <neftaliacosta@jobtify.email>
2023-08-19 04:33:49 -0600

    feat(init 3.php): add  stored procedure sp_MonthlyMovementCreate

--- inits/3.php
+++ inits/3.php
@@ -34,3 +34,60 @@ $o_mysql->custom("
         CONSTRAINT `FK_rinku_monthly_movements_rinku_employees` FOREIGN KEY (`employee_id`) REFERENCES `{$table_employees}` (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 ")->execute();
+
+
+// Print message
+Cli::e(
+    "Creating Stored Procedure `sp_MonthlyMovementCreate` in table:`{$table_monthly_movements}`",
+    ConsoleForegroundColors::Green
+);
+
+// Execute script
+$o_mysql->custom("
+    CREATE PROCEDURE `sp_MonthlyMovementCreate`(
+        IN `emp_id` INT,
+        IN `emp_deliveries` INT
+    )
+    LANGUAGE SQL
+    NOT DETERMINISTIC
+    CONTAINS SQL
+    SQL SECURITY DEFINER
+    COMMENT 'Add a monthly record for an employee'
+    BEGIN
+        DECLARE local_emp_id INT;
+        DECLARE local_emp_enum_rol INT;
+        DECLARE local_emp_base_salary INT;
+        DECLARE local_extra_salary INT;
+        DECLARE local_bonus_extra_hour INT;
+        DECLARE local_taxes INT DEFAULT 9;
+        DECLARE local_grocery_vouchers DECIMAL(10, 2);
+        
+        -- Gets employee information based on emp_id parameter
+        SELECT id, enum_rol, base_salary INTO local_emp_id, local_emp_enum_rol, local_emp_base_salary
+        FROM {$table_employees}
+        WHERE id = emp_id;
+        
+        -- Additional salary according to deliveries
+        SET local_extra_salary = emp_deliveries * 5;
+        
+        -- Additional salary according to role
+        IF local_emp_enum_rol = 1 THEN
+            SET local_bonus_extra_hour = (8 * 6 * 4) * 10;
+            SET local_extra_salary = local_extra_salary + local_bonus_extra_hour;
+        ELSEIF local_emp_enum_rol = 2 THEN
+            SET local_bonus_extra_hour = (8 * 6 * 4) * 5;
+            SET local_extra_salary = local_extra_salary + local_bonus_extra_hour;
+        END IF;
+        
+        -- Calculate taxes
+        IF local_emp_base_salary + local_extra_salary > 10000 THEN
+            SET local_taxes = local_taxes + 3;
+        END IF;
+        
+        -- Calculate grocery vouchers
+        SET local_grocery_vouchers = (local_emp_base_salary + local_extra_salary) * 0.04;
+        
+        INSERT INTO {$table_monthly_movements} (employee_id, deliveries, extra_salary, taxes, grocery_vouchers, creation_date)
+        VALUES (local_emp_id, emp_deliveries, local_extra_salary, local_taxes, local_grocery_vouchers, NOW());
+    END
+")->execute();
