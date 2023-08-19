96b93ac1ac5497f69eec3c197f9393e406fcfcf3
Neftali Acosta <neftaliacosta@jobtify.email>
2023-08-19 00:57:00 -0600

    feat(config.php): remove config file and move values to .env.dev file

--- App/Templates/default.php
+++ App/Templates/default.php
@@ -8,10 +8,10 @@
         <meta name="generator" content="Jekyll v4.1.1">
         <title>Template test</title>
 
-        <link rel="stylesheet" href="<?php echo __PATH__.'/resources/css/mystyle.css' ?>">
+        <link rel="stylesheet" href="<?php echo $_ENV['__PATH__'].'/resources/css/mystyle.css' ?>">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <style>
-            
+
         </style>
 </head>
 
@@ -30,7 +30,7 @@
     <div class="container">
 
 
-        <?php 
+        <?php
 
             /*
             Routes
